5d08d2a21690d29adfb673e166b56cd1af792fdb
MaRisa Jackson <mhljackson@gmail.com>
2014-04-17 22:52:43 -0500

    moveable pieces

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -5,7 +5,24 @@
 
   function initialize(){
     setupBoard();
-    $('.validSpace').click(selectPiece);
+    $('#board').on('click', '.validSpace.current.piece', selectPiece);
+    $('#board').on('click', 'td:not(.piece)', move);
+  }
+
+  function move(){
+    if ($('.selected').length > 0){
+      if($('.selected').hasClass('player1')){
+        $(this).addClass('player1 piece');
+        $('.selected').removeClass('player1 piece selected current');
+        $('.player1').removeClass('current');
+        $('.player2').addClass('current');
+      } else {
+        $(this).addClass('player2 piece');
+        $('.selected').removeClass('player2 piece selected current');
+        $('.player2').removeClass('current');
+        $('.player1').addClass('current');
+      }
+    }
   }
 
   function selectPiece(){
@@ -20,10 +37,10 @@
     $('tr:nth-child(2n - 1) td:nth-child(2n)').addClass('validSpace');
     var spaces = $('.validSpace');
     for (var i = 0; i < 12; i++){
-      $(spaces[i]).addClass('player2');
+      $(spaces[i]).addClass('player2 piece');
     }
     for(i = 20; i < 32; i++){
-      $(spaces[i]).addClass('player1');
+      $(spaces[i]).addClass('player1 piece current');
     }
   }
 
